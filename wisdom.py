4de2a0225064667269340c716fef80a0c25fcee7
Pascal Horton <pascal.horton@giub.unibe.ch>
2021-03-05 10:09:16 +0100

    Adding tuto code.

---
+++ wisdom.py
@@ -0,0 +1,31 @@
+import random
+
+# Add a quote below:
+quotes = [
+    "It always seems impossible until it is done.",
+    "Positive anything is better than negative nothing.",
+    "Life always offers you a second chance. It's called tomorrow.",
+    "We never really grow up, we only learn how to act in public.",
+    "He who laughs last didn’t get the joke.",
+    "Education is learning what you didn’t even know you didn’t know.",
+    "...Michael Bading...",
+    "...Ivana Cervenka...",
+    "...Manuela Ehret...",
+    "...Finn Marvin Feldmann...",
+    "...Juri Fitz...",
+    "...Michèle Grindat...",
+    "...Morris Grüter...",
+    "...Ann Christin Kogel...",
+    "...Haokun Liu...",
+    "...Michael Schäfer...",
+    "...Jonas Schild...",
+    "...Brigitta Schmid...",
+    "...Brian Marvin Schweigler...",
+    "...Sibylle Wilhelm...",
+    "...Peter Zweifel...",
+]
+
+print("\n" + random.choice(quotes))
+
+
+
